27f998a6b73627b1333e3aa750ffd673259173ed
maisydodge <maisy.dodge@snapav.com>
2018-08-02 12:42:37 -0500

    deleted commented out code

--- src/Validators.js
+++ src/Validators.js
@@ -54,32 +54,6 @@ export function fileSizeValidator(cell) {
   return response;
 }
 
-// //For firmware upgrade models - makes sure types are the same
-// export function modelValidator(modelArray) {
-//   const response = {
-//     isValid: true,
-//     notification: { type: "success", msg: "", title: "" }
-//   };
-//   if (modelArray[0] === undefined) {
-//     response.isValid = false;
-//     response.notification.type = "error";
-//     response.notification.msg = "Value must be selected!";
-//     response.notification.title = "Requested Value";
-//   } else {
-//     let type = modelArray[0].slice(0, 3);
-//     for (var i = 0; i < modelArray.length; i++) {
-//       if (modelArray[i].slice(0, 3) !== type) {
-//         response.isValid = false;
-//         response.notification.type = "error";
-//         response.notification.msg = "Model types must be the same!";
-//         response.notification.title = "Invalid Models";
-//         return response;
-//       }
-//     }
-//   }
-//   return response;
-// }
-
 //For firmware upgrade models - makes sure types are the same
 export function modelValidator(modelArray) {
   const response = {
